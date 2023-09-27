6db02a0a2aac21900f8022674d7e6840974b7626
leemiju <dlalwnjenny@naver.com>
2023-09-27 04:53:33 +0000

    test

--- chap4/p2.py
+++ chap4/p2.py
@@ -1,23 +1,29 @@
 import random
 
-hero. HP = random.randrange(50,100)
+hero.HP = random.randrange(50,100)
 monster.HP= random.randrange(50,100)
 att = random.randrange(-10,20)
+mtt = random.randrange(-10,20) 
 att.count=0 
 
 while( hero.HP >0 and monster.HP >0):
 
-    hero.HP = hero.HP - att
-    monster.HP = monster - att
+    hero.HP = hero.HP - att 
+    monster.HP = monster - mtt # left blood
 
-    if att <0:
-    continue
-      
+    if (att <0 and mtt<0):
+     print("hero(HP: "+(hero.HP+att)+ ", attct:"+ att + fail + "<-> monster(HP:"+( monster.HP+mtt)+", attct:"+ mtt+")"+ fail)  
+    elif(att<0 and mtt >0
+     print("hero(HP: "+(hero.HP+att)+ ", attct:"+ att + fail + "<-> monster(HP:"+ monster.HP+", attct:"+ mtt+")"success)  
+    elif(att>0 and mtt >0)
+     print("hero(HP: "+hero.HP+ ", attct:"+ att + success + "<-> monster(HP:"+ monster.HP+", attct:"+ mtt+")" success)  
+    elif(att>0 and mtt <0)
+     print("hero(HP: "+hero.HP+ ", attct:"+ att + fail + "<-> monster(HP:"+( monster.HP+mtt)+", attct:"+ mtt +")" fail)  
+          
     att.count = att.count + 1
-
-    if hero > monster:
+if hero.HP > monster.HP:
         print("hero win!!!")
-    else 
+        else 
         print("monster win!!!")
 
 print("-----------------------")
