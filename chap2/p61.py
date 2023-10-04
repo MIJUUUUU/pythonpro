088303d9f16a44169b61409ead15a288185fb023
leemiju <dlalwnjenny@naver.com>
2023-10-04 04:09:41 +0000

    test

--- chap2/p61.py
+++ chap2/p61.py
@@ -3,13 +3,9 @@ print('                      Trust Fund Buddy                      ')
 
 
 print("Totals your monthly spending so that your trust fund doesn't run out")
-print("<and you're forced to get a real job">
-                                           
-        print("Please enter the requested, monthly costs.  Since you're rich, ignore pennies and use only dollar amounts.")
-
-
-
-car = int(input ("Lamborghini Tune-Ups: "))
+print("<and you're forced to get a real job>")
+print("Please enter the requested, monthly costs.  Since you're rich, ignore pennies and use only dollar amounts.\n")
+tune = int(input("Lamborghini Tune-Ups: "))
 rent = int(input ("Manhattan Apartment: "))
 jet = int(input ("Private Jet Rental: "))
 gifts = int(input ("Gifts: "))
@@ -18,6 +14,4 @@ staff = int(input ("Staff (butlers, chef, driver, assistant): "))
 guru = int(input ("Perconal Guru and Coach: "))
 games = int(input ("Computer Games: "))
 
-
-total = car + rent + jet + gifts + food + staff + guru + games
- print("Grand total", total)
+print("\nGrand total : ",tune + rent + jet + gifts + food + staff + guru + games)
