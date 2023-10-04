e6781f141c043279901ce5c7b40a4ecc1ba2577b
leemiju <dlalwnjenny@naver.com>
2023-10-04 07:02:41 +0000

    test

--- chap4/p3.py
+++ chap4/p3.py
@@ -1,23 +1,28 @@
+import random
+
+
 print("              Welcome to 'Guess My Number!!!          ")
 print("I'm thinking of a number between 1 and 100.")
 print("Try to guess it in as few attempts as possible.")
 
-num = random.randrange(1,100)
+num = random.randrange(1,101)
 count =0
 
-mynum=""
 
-while num == mynum:
- print("Take a guess : ",mynum)
- if mynum < num:
+while num<=100 and num >=1:
+
+   mynum=int(input("Take a guess "))
+   count +=1
+   if mynum < num:
+    print("Upper . . .")
+   
+   elif  mynum > num:
     print("Lower . . .")
- elif mynum > num:
-    print("Higher . . .")
 
- count +=1
+   else:
+    break
 
- elif mynum == num:
-   print("You gueesed it ! The number was ", num)
-   print("And it only took you ", count , " tries !")
+print("You gueesed it ! The number was ", num)
+print("And it only took you ", count , " tries !")
  
 
