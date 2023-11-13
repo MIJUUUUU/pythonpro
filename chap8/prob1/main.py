3f1dd510b80976940ee77e9e6058042482a7e181
leemiju <dlalwnjenny@naver.com>
2023-11-13 02:45:43 +0000

    test

--- chap8/prob1/main.py
+++ chap8/prob1/main.py
@@ -8,13 +8,14 @@ print(text_file.read(5))
 
 print("Reading the entire file at once.") 
 whole_thing = text_file.read()
+
 print(whole_thing)
 
 print("Reading characters from a line.")
 text_file = open("read_it.txt", "r")
 print(text_file.readline(1))
 print(text_file.readline(5))
-
+print("")
 
 print("Reading one line at a time.")
 text_file = open("read_it.txt", "r")
@@ -28,6 +29,7 @@ lines = text_file.readlines()
 print(lines)
 for line in text_file:
     print(line)
+print("")
 
 print("Looping through the file. line by line.")
 text_file = open("read_it.txt", "r")
