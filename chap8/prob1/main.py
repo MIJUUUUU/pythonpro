527cfa5dba848d680efbd1227560e3b27b1aed5c
leemiju <dlalwnjenny@naver.com>
2023-11-13 02:40:14 +0000

    Test

--- chap8/prob1/main.py
+++ chap8/prob1/main.py
@@ -1,40 +1,39 @@
 text_file = open("read_it.txt", "r")
 
-  print("Opening and closing the file.")
+print("Opening and closing the file.")
 
-  print("Reading characters from the file.")
-  print(text_file.read(1))
-  print(text_file.read(5))
+print("Reading characters from the file.")
+print(text_file.read(1))
+print(text_file.read(5))
 
-  print("Reading the entire file at once.")
-  whole_thing = text_file.read()
-  	print(whole_thing)
+print("Reading the entire file at once.") 
+whole_thing = text_file.read()
+print(whole_thing)
 
-  print("Reading characters from a line.")
-  text_file = open("read_it.txt", "r")
-   print(text_file.readline(1))
-   print(text_file.readline(5))
+print("Reading characters from a line.")
+text_file = open("read_it.txt", "r")
+print(text_file.readline(1))
+print(text_file.readline(5))
 
-  print("Reading one line at a time.")
-  text_file = open("read_it.txt", "r")
-   print(text_file.readline())
-   print(text_file.readline())
-   print(text_file.readline())
 
-  print("Reading the entire file into a list.")
-  text_file = open("read_it.txt","r")
-  lines - text_file.readlines()
-  	print(lines)
-  	for line in text_file:
-      print(line)
+print("Reading one line at a time.")
+text_file = open("read_it.txt", "r")
+print(text_file.readline())
+print(text_file.readline())
+print(text_file.readline())
 
-  print("Looping through the file. line by line.")
-  text_file = open("read_it.txt", "r")
-   for line in text_file:
+print("Reading the entire file into a list.")
+text_file = open("read_it.txt","r")
+lines = text_file.readlines()
+print(lines)
+for line in text_file:
     print(line)
 
-text_file.close()
+print("Looping through the file. line by line.")
+text_file = open("read_it.txt", "r")
+for line in text_file:
+   print(line)
 
-  print("Press the enter key to exit.")
-   if print(" ")
-    break;
+text_file.close()
+print("Press the enter key to exit.")
+
