380577bd321c76797546cb64c5b0a540eb3e45a1
leemiju <dlalwnjenny@naver.com>
2023-11-15 06:21:44 +0000

    test

--- chap8/prob2/main.py
+++ chap8/prob2/main.py
@@ -6,22 +6,23 @@ text_file.write("Line 1\n")
 text_file.write("This is line2\n")
 text_file.write("That makes this line 3\n")
 text_file = open("write_it.txt", "r")
-print(text_file)
+print(text_file.read())
 print("")
 
 print("Creating a text file with the writeline<> method.")
 print("")
-print("Reading the newly created file.\n")
+print("Reading the newly created file.")
 text_file = open("write_it.txt", "w")
 lines = ["Line 1\n", "This is line 2\n", "That makes this line3\n"]
 text_file.writelines(lines)
-whole = text_file.read()
-print(whole)
+text_file = open("write_it.txt", "r")
+print(text_file.read())
+text_file.close()
 print("")
 print("")
 
 print("Press the enter key to exit.")
-put = input()
+
 while True:
   n = input("")
   if n == "":
