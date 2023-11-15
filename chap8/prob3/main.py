90d9ebc2dc8ab9e66252460da37294448e9fc574
leemiju <dlalwnjenny@naver.com>
2023-11-15 06:20:59 +0000

    test

--- chap8/prob3/main.py
+++ chap8/prob3/main.py
@@ -3,14 +3,13 @@ print("Input your string . . .")
 while True:
  text_file = open("write_it.txt", "w")
  text_file.write = input(">> ")	
- 
+  
  if (input(">> ") == 'Q'):
-	 break;
+	 
 
 print("Your inputs are below ..\n")
 text_file = open("write_it.txt", "r")
-for line in text_file:
-		print (line)
+print(text_file.read())
 text_file.close()
  
 
