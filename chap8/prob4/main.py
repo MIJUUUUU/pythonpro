9b531c8a441cbcdbd34d7a4cbea82cfa70b93656
leemiju <dlalwnjenny@naver.com>
2023-11-16 05:02:52 +0000

    Test

--- chap8/prob4/main.py
+++ chap8/prob4/main.py
@@ -1,13 +1,23 @@
-import cPickle
-print("Pickling lists.")
+import pickle
+import shelve
+
+print("Pickling lists.\n")
 print("Unpickling lists.")
 
 variety = ["sweet","hot","dill"]
-pickle_file = open("pickles1.dat","w")
-cPickle.dump(variety,pickle_file)
+pickle_file = open("pickles1.dat","wb")
+pickle.dump(variety,pickle_file)
 pickle_file.close()
 
-pickle_file = open("pickles1.dat","r")
-variety = cPickle.load(pickle_file)
+pickle_file = open("pickles1.dat","rb")
+variety = pickle.load(pickle_file)
 print(variety)
 pickle_file.close()
+
+print("Retrieving the lists from a sheleved file:")
+pickles = shelve.open("pickles2.dat")
+pickles["variety"] = ["sweet","hot","dill"]
+pickles.sync()
+for key in pickles.key()
+	print(key, "-", pickles[key])
+pickles.close()
